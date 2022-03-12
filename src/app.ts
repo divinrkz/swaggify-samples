5e97e8ae184cbd50236ca88ed3e91923d59b6968
Divin Irakiza <irakizadivin@gmail.com>
2022-03-12 23:34:50 +0200

    [feat]: app setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+
+
+const app = express();
+const port: number = 8080;
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );

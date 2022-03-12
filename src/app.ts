2c653341c5ff52f0558d6c78af313b3b75c762e7
Divin Irakiza <irakizadivin@gmail.com>
2022-03-12 23:41:26 +0200

    [chore]: add swaggify

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,16 @@
 import express from "express";
-import path from "path";
-
 
 const app = express();
-const port: number = 8080;
+const port: number = 4008;
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
 
-// define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     // render the index template
-    res.render( "index" );
+    return res.send('App opening');
 } );
 
 // start the express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
+    console.log(`server started at http://localhost:${ port }` );
 } );

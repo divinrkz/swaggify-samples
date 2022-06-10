6e6237955161d6e8af9d1a6571de7faea2fdba3c
divinirakiza <irakizadivin@gmail.com>
2022-06-10 20:54:51 +0200

    feat: remove unused files

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,5 @@ app.listen(5008, () => {
     console.log('Server is running 2');
 });
 
-app.get('/', (req: any, res: any) => {
-    return res.send('Server is running');
-});
 
 new Swaggify().setupExpress(app).swaggify();
